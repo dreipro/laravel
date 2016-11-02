e2d62f11724b6a445c231190eaaf695011e8f0af
Alexander von Renteln <alexander.renteln@gmail.com>
2016-11-02 13:07:39 +0100

    add log to Procfile and change become.sh

--- container_content/become.sh
+++ container_content/become.sh
@@ -1,35 +1,32 @@
 #!/bin/bash -xe
 
-#export USER_NAME="phon"
-#export USER_ID="1000"
-
-export USER_NAME=$1
-export USER_ID=$2
-export GROUP_ID=$3
-
-export GROUP_NAME=${USER_NAME}
 export USER_HOME="/app"
 
+export USER_NAME=app
+export GROUP_NAME=${USER_NAME}
 
-env
-
-exit
-
-
-# USER_NAME=$(stat --format=%U /app)
-# USER_ID=$(stat --format=%u /app)
-# USER=$(ls -ld /app | awk '{print $3}')
-#
-# OS - OSX: "Darwin", Linux: "Linux"
-# OS=$(uname -s)
+export USER_ID=$(stat --format=%u ${USER_HOME})
+export GROUP_ID=$(stat --format=%g ${USER_HOME})
 
 
-if ! id "${USER_NAME}" >/dev/null 2>&1; then
+echo USER_NAME:  $USER_NAME
+echo USER_ID:    $USER_ID
+echo USER_HOME:  $USER_HOME
+echo GROUP_NAME: $GROUP_NAME
+echo GROUP_ID:   $GROUP_ID
+
+if [ $USER_ID -eq 0 ]; then
+  exec bash
+fi
+
+
+if ! id "${USER_ID}" >/dev/null 2>&1; then
     echo "user does not exist - creating it..."
 
     groupadd -g ${GROUP_ID} ${GROUP_NAME}
     adduser --shell /bin/bash --uid ${USER_ID} --gid ${GROUP_ID} --no-create-home --disabled-password --home ${USER_HOME} ${USER_NAME}
 fi
+
 
 gosu ${USER} bash
 
