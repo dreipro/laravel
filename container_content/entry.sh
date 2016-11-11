83e12c7ac118528bc73aafe5fca0c3fbdc55394b
Alexander von Renteln <alexander.renteln@gmail.com>
2016-11-11 13:06:56 +0100

    add check to stop if no APP_KEY is defined

--- container_content/entry.sh
+++ container_content/entry.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "${APP_KEY}" ]; then
+  echo "Please set an APP_KEY environment variable first!"
+  exit -1
+fi
+
 /root/become.sh /init.sh
 
 cd / && /root/become.sh forego start -r
